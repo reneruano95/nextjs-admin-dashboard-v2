a3b69dcd97d4e44030b7fe777f4530258046b119
Rene Ruano <rene.ruano95@gmail.com>
2024-06-08 11:36:45 -0400

    feat:  add responsive behavior to sidebar

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -17,13 +17,16 @@ export default function ProtectedLayout({
       >
         <ResizablePanel
           defaultSize={25}
-          className="hidden md:block md:max-w-[75px] md:min-w-[75px] bg-slate-100 border-r lg:min-w-[75px] lg:max-w-[250px]"
+          className="hidden md:block md:max-w-[75px] md:min-w-[75px] bg-slate-100 lg:min-w-[75px] lg:max-w-[250px]"
         >
           <div className="flex h-full items-center justify-center p-6">
             <span className="font-semibold">Sidebar</span>
           </div>
         </ResizablePanel>
-        <ResizableHandle withHandle className=" hidden md:flex" />
+        <ResizableHandle
+          withHandle
+          className=" hidden md:flex hover:bg-slate-100 hover:border border-slate-200"
+        />
         <ResizablePanel defaultSize={75}>
           <div className="flex flex-col h-full items-center justify-center p-6">
             {children}
